79ac72640cc93bb0e683589a0b493ad44666c284
“bitaminkim” <bitamin.kim@gmail.com>
2017-11-09 14:09:50 +0800

    Update

---
+++ src/main/resources/generator/src/com/bitam/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.bitam.dao;
+
+import com.bitam.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
---
+++ src/main/resources/generator/src/com/bitam/pojo/User.java
@@ -0,0 +1,53 @@
+package com.bitam.pojo;
+
+public class User {
+    private Integer id;
+
+    private String username;
+
+    private String password;
+
+    private Byte age;
+
+    private String sex;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public Byte getAge() {
+        return age;
+    }
+
+    public void setAge(Byte age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex == null ? null : sex.trim();
+    }
+}
